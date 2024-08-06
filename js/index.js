6e9bc38d656f366765ebfbbc997b82f20c491e75
Sarmadjutt427 <110960231+Sarmadjutt427@users.noreply.github.com>
2024-08-06 05:19:30 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed('.txt', {
+        strings: ['Freelancer.', 'Front-End Developer.', 'Back-End Developer.'],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+});
